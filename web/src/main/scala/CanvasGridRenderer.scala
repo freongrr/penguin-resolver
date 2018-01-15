ecb90f9f3bb46487adb5e37d4b52474c86e2576c
Fabien Cortina <fabien.cortina@gmail.com>
2018-01-15 23:37:38 +0900

    Fix rendering of sides

--- web/src/main/scala/CanvasGridRenderer.scala
+++ web/src/main/scala/CanvasGridRenderer.scala
@@ -5,6 +5,14 @@ import org.scalajs.dom.html.Canvas
 
 object CanvasGridRenderer {
 
+  private val CellComparator = Ordering.by[Cell, Int] {
+    case EmptyCell(_, _) => 1
+    case OccupiedCell(_, _, content) => content match {
+      case Pawn() => 2
+      case ShapeSegment(_) => 3
+    }
+  }
+
   private val HorizontalMargin = 20
   private val VerticalMargin = 40
   private val HexaWidth = 40
@@ -13,25 +21,26 @@ object CanvasGridRenderer {
   def render(canvas: Canvas, grid: HexagonGrid): Unit = {
     val ctx = canvas.getContext("2d").asInstanceOf[CanvasRenderingContext2D]
 
-    for (y <- 0 until grid.height; x <- 0 until grid.width) {
-      val cell = grid(x, y)
-      val bounds = getCellBounds(y, x)
-      println(s"Rendering: $cell at left=${bounds.left}, top=${bounds.top}")
+    // Render cells sorted by depth
+    grid.cells sorted CellComparator foreach (renderCell(ctx, grid, _))
+  }
 
-      cell match {
-        case EmptyCell(_, _) => {
-          drawHexa(ctx, bounds, "rgb(0, 0, 255)")
-        }
-        case OccupiedCell(_, _, content) => {
-          content match {
-            case Pawn() => {
-              drawHexa(ctx, bounds, "rgb(255, 0, 255)")
-            }
-            case ShapeSegment(openSides) => {
-              drawBackground(ctx, bounds, "rgb(128, 128, 128)")
-              drawShapeSides(ctx, bounds, openSides)
-            }
-          }
+  private def renderCell(ctx: CanvasRenderingContext2D, grid: HexagonGrid, cell: Cell): Unit = {
+    val bounds = getCellBounds(cell.y, cell.x)
+    println(s"Rendering: $cell at $bounds")
+
+    cell match {
+      case EmptyCell(_, _) =>
+        drawBackground(ctx, bounds, "hsl(200, 100%, 50%)")
+        drawSides(ctx, bounds, Seq.empty, "hsl(200, 100%, 75%)", 1)
+      case OccupiedCell(_, _, content) => {
+        content match {
+          case Pawn() =>
+            drawBackground(ctx, bounds, "rgb(255, 0, 255)")
+            drawSides(ctx, bounds, Seq.empty, "rgb(0, 0, 0)", 1)
+          case ShapeSegment(openSides) =>
+            drawBackground(ctx, bounds, "rgb(192, 192, 192)")
+            drawSides(ctx, bounds, openSides, "rgb(0, 0, 0)", 1)
         }
       }
     }
@@ -83,25 +92,6 @@ object CanvasGridRenderer {
     HexaBounds(left, top, HexaWidth, HexaHeight)
   }
 
-  private def drawHexa(ctx: CanvasRenderingContext2D, bounds: HexaBounds, color: String): Unit = {
-    ctx.save()
-    ctx.strokeStyle = "rgb(0, 0, 0)"
-    ctx.fillStyle = color
-    ctx.lineWidth = 2
-    ctx.beginPath()
-    ctx.moveTo(bounds.leftQuarter, bounds.top)
-    ctx.lineTo(bounds.rightQuarter, bounds.top)
-    ctx.lineTo(bounds.right, bounds.middle)
-    ctx.lineTo(bounds.rightQuarter, bounds.bottom)
-    ctx.lineTo(bounds.leftQuarter, bounds.bottom)
-    ctx.lineTo(bounds.left, bounds.middle)
-    ctx.lineTo(bounds.leftQuarter, bounds.top)
-    ctx.closePath()
-    ctx.fill()
-    ctx.stroke()
-    ctx.restore()
-  }
-
   private def drawBackground(ctx: CanvasRenderingContext2D, bounds: HexaBounds, color: String): Unit = {
     ctx.save()
     ctx.fillStyle = color
@@ -118,11 +108,11 @@ object CanvasGridRenderer {
     ctx.restore()
   }
 
-  private def drawShapeSides(ctx: CanvasRenderingContext2D, bounds: HexaBounds, openSides: Seq[HexaDirection]): Unit = {
+  private def drawSides(ctx: CanvasRenderingContext2D, bounds: HexaBounds, openSides: Seq[HexaDirection], lineColor: String, lineWidth: Int): Unit = {
     def drawLine(x1: Double, y1: Double, x2: Double, y2: Double): Unit = {
       ctx.save()
-      ctx.strokeStyle = "rgb(0, 0, 0)"
-      ctx.lineWidth = 2
+      ctx.strokeStyle = lineColor
+      ctx.lineWidth = lineWidth
       ctx.beginPath()
       ctx.moveTo(x1, y1)
       ctx.lineTo(x2, y2)
@@ -134,9 +124,9 @@ object CanvasGridRenderer {
     if (!openSides.contains(UpLeft)) drawLine(bounds.left, bounds.middle, bounds.leftQuarter, bounds.top)
     if (!openSides.contains(Up)) drawLine(bounds.leftQuarter, bounds.top, bounds.rightQuarter, bounds.top)
     if (!openSides.contains(UpRight)) drawLine(bounds.rightQuarter, bounds.top, bounds.right, bounds.middle)
-    if (!openSides.contains(DownLeft)) drawLine(bounds.right, bounds.middle, bounds.rightQuarter, bounds.bottom)
+    if (!openSides.contains(DownLeft)) drawLine(bounds.leftQuarter, bounds.bottom, bounds.left, bounds.middle)
     if (!openSides.contains(Down)) drawLine(bounds.rightQuarter, bounds.bottom, bounds.leftQuarter, bounds.bottom)
-    if (!openSides.contains(DownRight)) drawLine(bounds.leftQuarter, bounds.bottom, bounds.left, bounds.middle)
+    if (!openSides.contains(DownRight)) drawLine(bounds.right, bounds.middle, bounds.rightQuarter, bounds.bottom)
   }
 
   private case class HexaBounds(x: Double, y: Double, width: Double, height: Double) {
